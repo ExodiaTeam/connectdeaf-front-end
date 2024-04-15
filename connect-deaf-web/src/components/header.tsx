b920d11ad0c8518038e02f62d1c6415246a8e8b5
Fernando Lima <eufernandoprogramador@gmail.com>
2024-04-15 19:26:01 -0300

    feat: Creating header component view

--- connect-deaf-web/src/components/header.tsx
+++ connect-deaf-web/src/components/header.tsx
@@ -1,15 +1,39 @@
-/*
-  - Alinhar corretamente os elementos (Search no centro, logo à esquerda e nav à direita)
-  - Escolher uma biblioteca de icones e substituir os icones atuais
-  - Fazer um hover para o nav
-  - Configurar a fonte e cores corretamente
-  - 
-*/
+import { MagnifyingGlass } from '@phosphor-icons/react'
+import { Link } from 'react-router-dom'
+
+import logo from '../assets/logo-branco-amarelo.svg'
 
 export function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="bg-primary-500 flex w-full items-center justify-between px-[4.5rem] py-6">
+      <Link to="/">
+        <img src={logo} alt="Logo CENOPS UFC" />
+      </Link>
+      <div className="flex w-[25.5rem] gap-2 rounded-md bg-white px-3 py-2">
+        <MagnifyingGlass size={24} className="text-primary-500" />
+        <input
+          type="text"
+          placeholder="Pesquisar por serviço, prestador..."
+          className="w-full bg-transparent outline-none"
+        />
+      </div>
+      <nav className="flex items-center justify-between gap-6">
+        <Link className="text-white transition-opacity hover:opacity-80" to="/">
+          SERVIÇOS
+        </Link>
+        <Link
+          className="font-bold text-white transition-opacity hover:opacity-80"
+          to="/sign-up"
+        >
+          CADASTRAR
+        </Link>
+        <Link
+          className="text-white transition-opacity hover:opacity-80"
+          to="/sign-in"
+        >
+          ENTRAR
+        </Link>
+      </nav>
+    </header>
   )
 }
