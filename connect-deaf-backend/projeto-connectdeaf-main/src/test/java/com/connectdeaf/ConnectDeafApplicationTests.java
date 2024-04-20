5562670d6ebde08478978cfd8edf46455c31f029
Luís Estevam Rosa Chaves <estevamchaves@alu.ufc.br>
2024-04-20 20:44:27 -0300

    feat: configuring maven and spring

---
+++ connect-deaf-backend/projeto-connectdeaf-main/src/test/java/com/connectdeaf/ConnectDeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectdeaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectDeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
