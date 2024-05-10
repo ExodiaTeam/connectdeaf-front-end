c114202cc528c2e8d5de8be5348203344d6f0fb3
Luís Estevam Rosa Chaves <estevamchaves@alu.ufc.br>
2024-05-09 23:27:29 -0300

    feat: criando crud de cadastro de user

--- connect-deaf-api/src/main/java/com/connectdeaf/controller/UserController.java
+++ connect-deaf-api/src/main/java/com/connectdeaf/controller/UserController.java
@@ -2,17 +2,24 @@ package com.connectdeaf.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.connectdeaf.model.Professional;
 import com.connectdeaf.model.User;
 import com.connectdeaf.services.UserService;
 
 import jakarta.validation.Valid;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("api/users")
@@ -25,7 +32,32 @@ public class UserController {
         try {
             return ResponseEntity.ok(this.userService.createUser(user));
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());  
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable UUID id) {
+        return this.userService.findById(id)
+                .map(user -> ResponseEntity.ok().body(user))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable UUID id, @Valid @RequestBody User user) {
+        return this.userService.updateUser(id, user)
+                .map(userUpdate -> ResponseEntity.ok().body(userUpdate))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
+        Optional<User> userOptional = this.userService.findById(id);
+        if (userOptional.isPresent()) {
+            this.userService.deleteUser(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
         }
     }
 }
